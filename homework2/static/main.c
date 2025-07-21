f6bab9fe5b138b29aebc29dc69f31deeaa89eafb
AleksSH97 <ashabalin@nao-team.com>
2025-07-21 19:39:49 +0300

    [+] homework2: add homework from second lesson

---
+++ homework2/static/main.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include <time.h>
+
+int array_fill_with_rand_nums(int *array, size_t len);
+int array_show(const int *array, size_t len);
+int sort_array(int *array, size_t len);
+
+#define SIZE_OF_ARRAY(array) (sizeof(array)/sizeof(array[0]))
+
+int main(void) {
+    int nums[10] = {0};
+
+    /* Update randomizer with new seed */
+    srand((unsigned int)time(NULL));
+
+    int err = array_fill_with_rand_nums(nums, SIZE_OF_ARRAY(nums));
+    if(err < 0) {
+        return 1;
+    }
+
+    err = array_show(nums, SIZE_OF_ARRAY(nums));
+    if(err < 0) {
+        return 1;
+    }
+
+    err = sort_array(nums, SIZE_OF_ARRAY(nums));
+    if(err < 0) {
+        return 1;
+    }
+
+    err = array_show(nums, SIZE_OF_ARRAY(nums));
+    if(err < 0) {
+        return 1;
+    }
+
+    return 0;
+}
