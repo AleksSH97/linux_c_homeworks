48ad8ca47964aac6e7e4384074fdcd7a87f482f0
AleksSH97 <ashabalin@nao-team.com>
2025-07-30 20:53:32 +0300

    [+] homework5: fix warnings for pipes example

---
+++ homework5/pipes/typeclient.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define FIFO_NAME "./fifofile"
+
+int main ()
+{
+  FILE * f;
+  char ch;
+  f = fopen(FIFO_NAME, "r");
+  do
+  {
+    ch = fgetc(f);
+    putchar(ch);
+  } while (ch != 'q');
+  fclose(f);
+  unlink(FIFO_NAME);
+  return 0;
+}
