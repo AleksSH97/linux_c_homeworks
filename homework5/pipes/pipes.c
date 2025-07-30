2fdeacdef5279ab4d64f74083133dad875c7d488
AleksSH97 <ashabalin@nao-team.com>
2025-07-30 21:15:59 +0300

    [+] homework5: add grandchild in pipes example

--- homework5/pipes/pipes.c
+++ homework5/pipes/pipes.c
@@ -6,26 +6,36 @@
 int main (int argc, char * argv[])
 {
   int pipedes[2];
-  pid_t pid;
-  pid_t pid_2;
   pipe(pipedes);
-  pid = fork();
-  pid_2 = fork();
-  if ( pid > 0 )
-  {
-    char  *str = "String passed via pipe\n";
+  pid_t pid = fork();
+
+  if(pid > 0) {
     close(pipedes[0]);
+    char  *str = "String from parent\n";
     write(pipedes[1], (void *) str, strlen(str) + 1);
     close(pipedes[1]);
-  }
-  else
-  {
-    char buf[1024];
-    int len;
-    close(pipedes[1]);
-    while ((len = read(pipedes[0], buf, 1024)) != 0)
-    write(2, buf, len);
-    close(pipedes[0]);
+  } else {
+    pid_t pid2 = fork();
+
+    if(pid2 > 0) {
+        close(pipedes[1]);
+        char buf[1024];
+        int len;
+        len = read(pipedes[0], buf, sizeof(buf));
+        if (len > 0) {
+            write(1, "Child read: ", 12), write(1, buf, len);
+        }
+        close(pipedes[0]);
+    } else {
+        close(pipedes[1]);
+        char buf[1024];
+        int len;
+        len = read(pipedes[0], buf, sizeof(buf));
+        if(len > 0) {
+            write(1, "Grandchild read: ", 17), write(1, buf, len);
+        }
+        close(pipedes[0]);
+    }
   }
   return 0;
 }
